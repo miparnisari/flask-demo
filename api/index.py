bd18003960103da02f1c4e632dfc3aa85db7baf3
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2023-07-24 20:34:43 -0700

    maybe this works?

--- api/index.py
+++ api/index.py
@@ -6,6 +6,12 @@ jwks_file = open("jwks.json", "r")
 jwks_contents = jwks_file.read()
 jwks_file.close()
 
+response_file = open("response.txt", "r")
+response_contents = response_file.read()
+response_file.close()
+
 @app.route("/<user_id>/.well-known/jwks.json")
 def jwks(user_id):
-    return jwks_contents
+    resp = flask.Response()
+    resp.set_data(value=response_contents)
+    return resp
