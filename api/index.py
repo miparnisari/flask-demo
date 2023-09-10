8ccfbec8ac2045c0ec10473e909bde1160b3d8a3
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2023-09-09 20:01:03 -0700

    update

--- api/index.py
+++ api/index.py
@@ -2,10 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-jwks_file = open("jwks.json", "r")
-jwks_contents = jwks_file.read()
-jwks_file.close()
-
-@app.route("/<user_id>/.well-known/jwks.json")
-def jwks(user_id):
-    return jwks_contents
+@app.route("/")
+def index():
+    return "hi"
